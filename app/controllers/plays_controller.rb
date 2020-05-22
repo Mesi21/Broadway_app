1d432e93c1ac46ed815ea659f083431afd519b37
Mesi21 <mse212002@gmail.com>
2020-05-22 20:12:01 +0300

    :art: added new method and a form partial

--- app/controllers/plays_controller.rb
+++ app/controllers/plays_controller.rb
@@ -1,6 +1,18 @@
 class PlaysController < ApplicationController
 
-    def index
-    end
+  def index
+  end
 
+  def new
+      @play = Play.new
+  end
+
+  def create
+      @play = Play.new(play_params)
+  end
+
+  private
+    def play_params
+      params.require(:play).permit(:title, :description, :director)
+    end
 end
