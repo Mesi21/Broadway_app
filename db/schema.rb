2c5b1877e478f7f408d989489158f360507a4494
Mesi21 <mse212002@gmail.com>
2020-05-29 17:00:44 +0300

    :art: added review model and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_125426) do
+ActiveRecord::Schema.define(version: 2020_05_29_135731) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -30,6 +30,15 @@ ActiveRecord::Schema.define(version: 2020_05_26_125426) do
     t.string "play_img_content_type"
     t.integer "play_img_file_size"
     t.datetime "play_img_updated_at"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "play_id"
   end
 
   create_table "users", force: :cascade do |t|
