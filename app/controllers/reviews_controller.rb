4cf7f6d820ff3b62e0c8f4e259af18fa329035a5
Mesi21 <mse212002@gmail.com>
2020-06-16 20:02:56 +0300

    :art: added review controller

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  before_action :find_play
+
+  def new
+      @review = Review.new
+  end
+
+  def create
+      @play = Play.find(params[:play_id])
+      @review = Review.new(review_params)
+      @review.play_id = @play.id
+      @review.user_id = current_user.id
+
+      if @review.save
+        redirect_to play_path(@play)
+      else
+        render 'new'
+      end
+  end
+
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+
+  def find_play
+    @play = Play.find(params[:play_id])
+  end
+end
