bed2debe2d820c47ac9894d79ce36feef9939483
Mesi21 <mse212002@gmail.com>
2020-06-17 18:12:53 +0300

    :rotating_light: fixed linter warning for aling keys

--- app/models/play.rb
+++ app/models/play.rb
@@ -6,6 +6,7 @@ class Play < ApplicationRecord
   has_many :reviews
 
   has_attached_file :play_img, styles: { play_index: '250x350>', 
-    play_show: '325x475>' }, default_url: '/images/:style/missing.png'
+                                         play_show: '325x475>' }, 
+                                         default_url: '/images/:style/missing.png'
   validates_attachment_content_type :play_img, content_type: %r{\Aimage/.*\Z}
 end
