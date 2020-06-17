0c5395a132f4830ce4e903f36405d76aaedb5f0a
stickler-ci <support@stickler-ci.com>
2020-06-17 15:15:09 +0000

    Fixing style errors.

--- app/models/play.rb
+++ app/models/play.rb
@@ -5,8 +5,8 @@ class Play < ApplicationRecord
   belongs_to :category
   has_many :reviews
 
-  has_attached_file :play_img, styles: { play_index: '250x350>', 
-                                         play_show: '325x475>' }, 
-                                         default_url: '/images/:style/missing.png'
+  has_attached_file :play_img, styles: { play_index: '250x350>',
+                                         play_show: '325x475>' },
+                               default_url: '/images/:style/missing.png'
   validates_attachment_content_type :play_img, content_type: %r{\Aimage/.*\Z}
 end
